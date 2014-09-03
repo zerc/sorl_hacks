f36448e8fb0f0490fdd2bdd9ed6fa671d5e7d341
zero13cool <zero13cool@yandex.ru>
2014-09-04 00:20:49 +0400

    release 0.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-sorl-hacks',
-    version='0.1',
+    version='0.1.10',
     author='Vladimir Savin',
     author_email='zero13cool@yandex.ru',
 
